8c24e4e0b7065d33c463e8f79d704ce4f1cb3c0a
migbro <miguel.a.brown@gmail.com>
2015-12-07 17:06:14 -0600

    WIP

--- settings.py
+++ settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'viewer',
+    'mw_viewer',
 )
 
 MIDDLEWARE_CLASSES = (
--- urls.py
+++ urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^mw_lims/', include('viewer.urls')),
+    url(r'^mw_viewer/', include('mw_viewer.urls')),
     url(r'^', include('django.contrib.auth.urls'))
 ]
